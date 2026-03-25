cfd1b5c2dbded71869e4a75a2e901699b7a8c6ca
Bhagyashree <bhagyashree.888888@gmail.com>
2026-03-25 18:35:20 +0530

    25/3/26 commit

--- tests/endToend.spec.js
+++ tests/endToend.spec.js
@@ -9,21 +9,41 @@ test("end to end testing demo",async ({browser}) => {
     await page .locator("#userEmail").fill("Admin12345678@gmail.com");
     await page.locator("#userPassword").fill("Admin@123");
     await page.locator("#login").click();
-   // await page.getByText("Add To Cart").click();
+   
    await page.waitForLoadState('networkidle');
-   const productsName = await page.locator(".card-body b").allTextContents();
+   await page.locator(".card-body b").first().waitFor();
+   const productsNameList = await page.locator(".card-body b").allTextContents();
    // const product = await page.locator(".container").allTextContents(); //whole container text it extracted
-   console.log(productsName);
+   console.log(productsNameList);
+   const ProductName = "ZARA COAT 3";
+   const count = await page.locator(".card-body b").count();
+   
+   const products = page.locator(".card-body");
+   for(let i=0;i<count;i++){
+    //await page.locator(".card-body b").nth(i).textContent() === ProductName wrong u written in if condition
+    if(await products.nth(i).locator("b").textContent() === ProductName){
+     await products.nth(i).locator("text= Add To Cart").click();
+     break;
+   }
+   }
+   await page.locator("[routerlink*='cart']").click();
+   await page.locator("text=Buy Now").click();
+  
+   await page.locator(".txt").nth(1).fill("1234");
+    await page.locator(".txt").nth(2).fill("riya")
+     await page.locator(".txt").nth(3).fill("rahulshettyacademy");
+    await page.locator("[type='submit']").click();
 
-//    const count = await page.locator(".card-body ").count();
-//    console.log(count);
-//    let p="ZARA COAT 3 ";
-//    for(let i=0;i<count;i++){
-//   if(await page.locator(".card-body b").nth(i).textContent()===p){
-//     await page.locator(".card-body button").nth(i).click();
-//     break;
-//    }
-//   //await page.pause();
-//    await page.waitForTimeout(2000); // waits for 2 seconds
-// await page.close();
-});
+    await page.locator("[placeholder='Select Country']").pressSequentially("ind");
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const  dropdownCount = await dropdown.locator("button").count();
+    console.log(dropdownCount);
+    for(let i=0;i<dropdownCount;i++){
+        const text = await dropdown.locator("button").nth(i).textContent(); 
+        if(text === " India"){
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }   
+      await page.pause();
+}});
